47e8e451ef1676b5925c5aace1e2a07fea807498
OseiasDomingues <oseiasdomingues9@gmail.com>
2021-10-11 10:35:16 -0300

    project spring created

---
+++ src/main/java/com/devchallenge/apirestbooks/ApiRestBooksApplication.java
@@ -0,0 +1,13 @@
+package com.devchallenge.apirestbooks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestBooksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestBooksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devchallenge/apirestbooks/ApiRestBooksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devchallenge.apirestbooks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestBooksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
